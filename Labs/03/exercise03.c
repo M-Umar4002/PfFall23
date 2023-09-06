2b4941c62156a4129466dd25fb01c37685139bd3
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-09-06 23:10:39 +0500

    Create exercise03.c

---
+++ Labs/03/exercise03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int originalPrice, discountPercentage;
+    
+    printf("\n== Sale Price Calculating Program ==\n");
+    printf("\n-- Taking Inputs --\n");
+    
+    printf("\nEnter original price in Rs. : ");
+    scanf("%d", &originalPrice);
+    
+    printf("Enter discount percentage : ");
+    scanf("%d", &discountPercentage);
+    
+    printf("\n The sale price is Rs. %d.\n", originalPrice - (discountPercentage * originalPrice)/100);
+}
