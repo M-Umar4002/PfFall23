346799f616fae56d3421eaf7bb013f08960cbe0a
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-09-08 22:56:48 +0500

    Update exercise02.c

--- Labs/03/exercise02.c
+++ Labs/03/exercise02.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-int main() {
-	int height = 1, length = 2, width = 3;
+void main() {
+	int height, length, width;
+	
 	printf("\n== Volume Finding Program ==\n");
-	printf("-- Taking Inputs --\n");
+	printf("\n-- Taking Inputs --\n");
+	
 	printf("\nEnter container's height : ");
 	scanf("%d", &height);
+	
 	printf("Enter container's length : ");
 	scanf("%d", &length);
+	
 	printf("Enter container's width : ");
 	scanf("%d", &width);
-	int volume = height * length * width;
-	printf("\nThe volume of the container is : %d\n", volume);
+	
+	printf("\nThe volume of the container is %d cubic units. \n", height * length * width);
 }
