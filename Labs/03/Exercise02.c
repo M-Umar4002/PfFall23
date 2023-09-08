af2b9b142b8b92583dc0c73f686c6506b068f901
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-09-08 23:05:53 +0500

    Update Exercise02.c

--- Labs/03/Exercise02.c
+++ Labs/03/Exercise02.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int main () {
+void main () {
 	float base, perpendicular;
+	
 	printf("\n== Hypotenuse Finding Program ==\n");
 	printf("\n-- Taking Inputs --\n");
+	
 	printf("\nEnter Triangle's base : ");
 	scanf("%f", &base);
+	
 	printf("Enter Triangle's perpendicular : ");
 	scanf("%f", &perpendicular);
+	
 	float hypotenuse = sqrt((perpendicular * perpendicular) + (base * base));
 	printf("\nThe hypotenuse of the triangle is %f units.\n", hypotenuse);
 }
