4daac540a389292e7c843f5548772b5a4b7ee941
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-09-12 22:52:20 +0500

    Update q02.c

--- Labs/04/q02.c
+++ Labs/04/q02.c
@@ -12,25 +12,28 @@
 
 void main(){
 
-	int n;
-	printf("\nEnter any positive whole number to print n '*' n times:");
+	int n; // to take user's input
+	
+	printf("\nEnter any positive whole number to print n '*' n times : ");
 	scanf("%d", &n);
+	
+	printf("\n");
 
-	int count = 1;
+	int count = 1; // counter for moving on new line
 
 	while(count <= n){
 
-		int i = n;
+		int i = n; // counter for printing "*"
 		
 		while(i > 0){
       
 			printf("*");
 			i = i - 1;
 		
-    }
+        }
 		
 		count = count + 1;
-		printf("\n");
-  }
+		printf("\n"); // changing the line
+    }
 
-}//end main
+} //end main
