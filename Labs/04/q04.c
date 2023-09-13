9b0db02ed146bc61294896e9fa912a9d59ea6c0d
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-09-13 14:38:53 +0500

    Create q04.c

---
+++ Labs/04/q04.c
@@ -0,0 +1,34 @@
+/*
+* Programmer: Muhammad Umar
+* Date: 12/9/2023
+* Description: Return the solutions using the quadratic formula
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    
+    printf("\n-- Taking Inputs --\n\n");
+    
+    int a, b, c; // to get user's input
+    
+    printf("Enter coefficient of the term with power 2 : ");
+    scanf("%d", &a);
+    
+    printf("Enter coefficient of the term with power 1 : ");
+    scanf("%d", &b);
+    
+    printf("Enter the constant : ");
+    scanf("%d", &c);
+    
+    float d, x1, x2; // d for discriminant, x1 for Solution 1 and x2 for solution 2
+    
+    d = sqrt((b * b) - 4 * a * c);
+    
+    x1 = (- b + d) / (2 * a);
+    x2 = (- b - d) / (2 * a);
+    
+    printf("\nSolution 1 : %.2f", x1);
+    printf("\nSolution 2 : %.2f\n", x2);
+}
