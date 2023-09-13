0262ec90084c740e17ea4c80d2ed74152990daaa
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-09-13 15:25:33 +0500

    Create q05.c

---
+++ Labs/04/q05.c
@@ -0,0 +1,44 @@
+/*
+* Programmer: Muhammad Umar
+* Date: 12/9/2023
+* Description: By taking input DOB of two persons, check who is older
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    
+    printf("\n-- Taking Inputs --\n\n");
+    
+    int d1, m1, y1, d2, m2, y2; // to take input DOB of two persons
+    
+    printf("Enter first person's date, month and year of birth in format (dd/mm/yyyy) : ");
+    scanf("%d/%d/%d", &d1, &m1, &y1);
+    
+    printf("\n");
+    
+    printf("Enter second person's date, month and year of birth in format (dd/mm/yyyy) : ");
+    scanf("%d/%d/%d", &d2, &m2, &y2);
+    
+    printf("\n");
+    
+    if(y1 < y2){
+        printf("First person is older!");
+    } else if(y1 == y2){
+        if(m1 < m2){
+            printf("First person is older!");
+        } else if(m1 == m2){
+            if(d1 < d2){
+                printf("First person is older!");
+            } else {
+                printf("Second person is older!");
+            }
+        } else{
+            printf("Second person is older!");
+        }
+    } else{
+        printf("Second person is older!");
+    }
+  
+} // end point
