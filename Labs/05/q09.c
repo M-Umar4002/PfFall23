acfba5ad5c88002f44d558ef6177803655ac2dbb
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-10-01 03:52:41 +0500

    Create q09.c

---
+++ Labs/05/q09.c
@@ -0,0 +1,41 @@
+/*
+* Programmer: Muhammad Umar
+* Date: 19/9/2023
+* Description: Compute roots of quadratic equation based on the nature of discriminant.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    
+    int a, b, c; // to get user's input
+    
+    printf("Enter coefficient of the term with power 2 : ");
+    scanf("%d", &a);
+    
+    printf("Enter coefficient of the term with power 1 : ");
+    scanf("%d", &b);
+    
+    printf("Enter the constant : ");
+    scanf("%d", &c);
+    
+    float d, x1, x2; // d for discriminant, x1 for Solution 1 and x2 for solution 2
+    
+    // calculating discriminant
+    d = (b * b) - 4 * a * c;
+    
+    // calculating roots
+    x1 = (- b + sqrt(d)) / (2 * a);
+    x2 = (- b - sqrt(d)) / (2 * a);
+    
+    // checking if determinant is >, = , < 0
+    if (d > 0) {
+        printf("\nSolution 1 : %.2f", x1);
+        printf("\nSolution 2 : %.2f\n", x2);
+    } else if (d == 0) {
+        printf("\nSolution : %.2f", x1);
+    } else {
+        printf("\nRoots are imaginary");
+    }
+}
