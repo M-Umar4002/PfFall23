cba28b85f8cafd2cd91b54bff05d328172758ad8
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-10-25 00:55:40 +0500

    Update q06.c

--- Labs/08/q06.c
+++ Labs/08/q06.c
@@ -1,29 +1,33 @@
 /*
 * Programmer: Muhammad Umar
 * Date: 24/10/2023
-* Description: Take password of at least 8 characters from user. Compare it with the passord "Secure123" and printing the output according to it.
+* Description: Take an array of integers and its size and calculate the sum, maximum, and minimum values in the array by making a function.
 */
 
 #include <stdio.h>
 
+//to find the sum, minimum and maximum value of the array by taking array and its size
 void processArray(int arr[], int size){
     arr[size];
     
-    int min = arr[0]; int max = arr[0], sum = 0;
+    int min = arr[0], max = arr[0], sum = 0;
     
+    //to calculate te sum and find minimum and maximum value
     for(int i = 0; i < size; i++){
-        sum += arr[i];
+        sum += arr[i]; //calculating the sum
         
+        //finding maximum value
         if(arr[i] > max){
             max = arr[i];
         }
         
+        //finding minimum value
         if(arr[i] < min){
             min = arr[i];
         }
     }
-    printf("Array Sum = %d\nMaximum value = %d\nMinimum value = %d", sum, max, min);
-}
+    printf("\nArray Sum : %d\nMaximum Value : %d\nMinimum Value : %d\n", sum, max, min);
+} //end processArray
 
 void main(){
     
@@ -31,7 +35,9 @@ void main(){
 
     printf("\nEnter array size(total number of elements) : "); //taking size as user's input
     scanf("%d", &size);
-
+    
+    printf("\n");
+    
     int arr[size];
 
     //asking user for all the elements and storing them in an array
@@ -41,4 +47,4 @@ void main(){
     }
     
     processArray(arr, size);
-}
+} //end main
