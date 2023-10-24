c686fabe18e3ea2d44410ecdbb7c2d3978cf0832
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-10-24 23:20:39 +0500

    Create q05.c

--- Labs/08/q05.c
+++ Labs/08/q05.c
@@ -1,35 +1,39 @@
 /*
 * Programmer: Muhammad Umar
 * Date: 24/10/2023
-* Description: Take password of at least 8 characters from user. Compare it with the passord "Secure123" and printing the output according to it.
+* Description: Take numeric part of the car's number and day number of the week from user. Check whether the car should be used or not by making the function and returning a value from it. The car could only be used if both (the car's number and the day's number) are even or odd.
 */
 
 #include <stdio.h>
 
+//to decide whether the car should be used or not by taking numeric part of car number and day number of the week
 int decideCarUsage(int carNum, int dayNum){
     
+    //checking for odd and even day numbers separately
     if((dayNum == 1 || dayNum == 3 || dayNum == 5 || dayNum == 7) && carNum % 2 != 0){
         return 1;
     } else if((dayNum == 2 || dayNum == 4 || dayNum == 6) && carNum % 2 == 0) {
         return 1;
     }
     return 0;
-}
+} //end decideCarUsage
 
 void main(){
 
     int carNum, dayNum;
 
-    printf("\nEnter numeric part of your car's number : ");
+    printf("\nEnter numeric part of your car's number : "); //taking numeric part of car's number as user's input
     scanf("%d", &carNum);
-
+    
+    //taking day number of the week as user's input until it lies between 1 to 7
     do{
         printf("Enter the day number of the week : ");
         scanf("%d", &dayNum);
     } while(dayNum < 1 && dayNum > 7);
 
     int checkValue = decideCarUsage(carNum, dayNum);
-
+    
+    //to print the output based on the value returned by the function
     if(checkValue == 1){
         printf("\nCar should be used!\n");
     } else {
