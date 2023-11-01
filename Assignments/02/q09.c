af49ccec631f16693272b33867937e540ed0b4dc
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-11-01 06:26:33 +0500

    Update q09.c

--- Assignments/02/q09.c
+++ Assignments/02/q09.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include <math.h>
 
 void inwards(int arr[][100], int num){
     
     int total = 1;
     
-    for(int x = 0; x <= num/2 && total <= num*num; x++){
+    for(int x = 0; x <= num/2; x++){
         
         for(int j = x; j < num-x; j++){
             arr[x][j] = total++;
         }
         
+        if(total == num * num + 1) break;
+            
         for(int i = x+1; i < num-x-1; i++){
             arr[i][num-x-1] = total++;
         }
@@ -33,9 +34,61 @@ void inwards(int arr[][100], int num){
     }
 }
 
+void outwards(int arr[][100], int num){
+    
+    int total = 1;
+    
+    for(int x = num/2; x >= 0; x--){
+    
+        for(int i = x; i < num - x; i++){
+            arr[i][num - x - 1] = total++;
+        }
+        
+        if(total - 1 == 1) break;
+        
+        for(int j = x + 1; j > 0; j--){
+            arr[x + 1][j] = total++;
+        }
+        
+        for(int i = x; i >= x; i--){
+            arr[i][x - 1] = total++;
+        }
+        
+        for(int j = x - 1; j <= x + 1; j++){
+            arr[x - 1][j] = total++;
+        }
+        
+        // for(int j = num/2; j < num-(num/2)-1; j++){
+        //     arr[num/2][j] = total++;
+        // }
+            
+        // for(int i = (num/2)+1; i < num-(num/2)-1; i++){
+        //     arr[i][num-(num/2)-1] = total++;
+        // }
+        
+        // for(int j = num-x-1; j >= x; j--){
+        //     arr[num-x-1][j] = total++;
+        // }
+        
+        // for(int i = num-x-2; i > x; i--){
+        //     arr[i][x] = total++;
+        // }
+        
+    }
+    
+    for(int i = 0; i < num; i++){
+        for(int j = 0; j < num; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void main(){
     
     int num;
+    
+    printf("\n");
     
     do{
         printf("Enter the dimension of matrix : ");
@@ -45,4 +98,8 @@ void main(){
     int arr[num][num];
     
     inwards(arr, num);
+    
+    printf("\n");
+    
+    outwards(arr, num);
 }
