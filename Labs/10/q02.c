2362e45737b837c768d567fd5178f080d65cbf6b
M-Umar4002 <134300686+M-Umar4002@users.noreply.github.com>
2023-11-14 14:58:35 +0500

    Update q02.c

--- Labs/10/q02.c
+++ Labs/10/q02.c
@@ -1,55 +1,67 @@
+/*
+* Programmer: Muhammad Umar
+* Date: 11/10/2023
+* Description: Create a program that dynamically allocates memory for a 2D array of integers. Write a function that accepts a pointer to a pointer to an integer and fills the 2D array with consecutive prime numbers.
+*/
+
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
+//to check if the number is prime or not
 bool isPrime(int num){
 	
-	int i;
-	
-	for(i = 2; i <= sqrt(num); i++){
+	for(int i = 2; i <= sqrt(num); i++){
 		if(num % i == 0){
 			return false;
 		}
 	}
 	return true;
-}
+} //end isPrime
 
+//to print all the prime numbers
 void printPrime(int **arr, int rows, int cols){
 	
-	int i;
-	int j, k, num = 2;
-	for(i = 0; i < rows; i++){
-		for(j = 0; j < cols; j++){
+	//to fill the 2D array with prime numbers
+	for(int i = 0, num = 2; i < rows; i++){
+		for(int j = 0; j < cols; j++){
 			if(isPrime(num)){
 				*(*(arr + i) + j) = num;
+			} else {
+			    j--;
 			}
 			num++;
 		}
 	}
 	
-	for(i = 0; i < rows; i++){
-		for(j = 0; j < cols; j++){
-			printf("%d", *(*(arr + i) + j));
+	//to print the output
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			printf("%d ", *(*(arr + i) + j));
 		}
 		printf("\n");
 	}
-}
+	
+} //end printPrime
 
 void main(){
 	
-	int rows, cols, i;
+	int rows, cols;
 	
-	printf("Rows : ");
+	printf("Enter total no. of rows : "); //taking no. of rows as user's input
 	scanf("%d", &rows);
 	
-	printf("Columns : ");
+	printf("Enter total no. of columns : "); //taking no. of columns as user's input
 	scanf("%d", &cols);
 	
-	int **arr = (int**) calloc(rows, sizeof(int*));
-	
-	for(i = 0; i < cols; i++){
-		*(arr + i) = (int*) calloc(cols, sizeof(int));
+	//allocating memory dynamically for a 2D array
+	int **arr = (int**) malloc(rows * sizeof(int*));
+	for(int i = 0; i < cols; i++){
+		*(arr + i) = (int*) malloc(cols * sizeof(int));
 	}
 	
+	printf("\n");
+	
 	printPrime(arr, rows, cols);
-}
+} //end main
